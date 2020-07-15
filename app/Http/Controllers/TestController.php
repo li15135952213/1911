0816616e4dc81f26352be8c519f4de2131b92b4d
liqin <2868838052@qq.com>
2020-07-15 11:54:04 +0000

    Guzzle获取access_token

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//引入guzzle类
+use GuzzleHttp\Client;
+use Illuminate\Support\Str;
 
 class TestController extends Controller
 {
+//获取access_token方法
     public function getwxToken(){
         $appid = 'wx815bc65a7b56df0b';
         $appsec = '2c7cd8d787a22618865e1241705cdb23';
@@ -14,6 +18,7 @@ class TestController extends Controller
         echo $cont;
     }
 
+//使用curl获取access_token
     public function getwxToken2(){
         $appid = 'wx815bc65a7b56df0b';
         $appsec = '2c7cd8d787a22618865e1241705cdb23';
@@ -37,4 +42,33 @@ class TestController extends Controller
 
     }
 
+//使用Guzzle获取access_token
+    public function getGuzzleToken(){
+        //appid
+        $appid = 'wx815bc65a7b56df0b';
+        //appsecret
+        $appsec = '2c7cd8d787a22618865e1241705cdb23';
+        //获取access_token的接口
+        $url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsec;
+        //实例化guzzle
+        $client = new Client();
+        //发送请求
+        $response = $client->request('GET',$url);
+        //获取响应的主体部分
+        $body = $response->getBody();
+        //打印 输出
+        echo $body;
+    }
+
+    //自己生成token
+    public function getAccessToken(){
+        $token = Str::random(32);
+        echo $token;
+
+        $data=[
+            'token' => $token,
+            'expire_in' => 7200
+        ];
+        echo json_encode($data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//access_token路由
 Route::get('/wx/token','TestController@getwxToken');
+
+//curl获取access_token
 Route::get('/wx/token2','TestController@getwxToken2');
+
+//使用Guzzle获取access_token
+
+Route::get('/wx/token3','TestController@getGuzzleToken');
+
+//自己生成token
+Route::get('/api/token','TestController@getAccessToken');
